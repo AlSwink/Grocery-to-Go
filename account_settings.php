0643ea9e32fba465b361ea628f655557c68e4da8
Joshua Settlemoir <joshua_settlemoir@yahoo.com>
2015-03-11 16:47:57 -0500

    Update account_settings.php
    Made some changes to the Settings page. The session variable will still need to be edited to allow for the email address and card number, and I wasn't sure what we wanted to be editable on this pae.

--- account_settings.php
+++ account_settings.php
@@ -6,15 +6,17 @@
    if(isset($_GET['Accfname'])){
       $repfname=$_GET['Accfname'];
       $replname=$_GET['Acclname'];
-      $repcolor=$_GET['AccColor'];
-      $reptitle=$_GET['AccTitle'];
-      $repimage=$_GET['AccImage'];
+      $repemail=$_GET['AccEmail']
+      $repcard=$_GET['AccCard'] 
+    //  $repcolor=$_GET['AccColor'];
+   //   $reptitle=$_GET['AccTitle'];
+    //  $repimage=$_GET['AccImage'];
       $data=file("customer-account-info.txt");
       $search=$_SESSION['user'].";".$_SESSION['password'].";";
       $result='';
       foreach($data as $line){
          if(substr($line,0,strlen($search))==$search){
-            $result .=$search.$repfname.";".$replname.";".$repcolor.";".$reptitle.";".$repimage.PHP_EOL;
+            $result .=$search.$repfname.";".$replname.";".$repemail.";".$repcard.PHP_EOL;
          }
          else{
             $result .=$line;
@@ -24,24 +26,21 @@
 
    }
 
-echo "<title>{$_SESSION['title']}</title>
-        <head>{$_SESSION['title']}</head>
-        <body bgcolor={$_SESSION['color']}>
-        <p>{$_SESSION['user']}has logged in with{$_SESSION['password']}</p>
-        <img  src=";
-  echo  $_SESSION['image'];
-  echo  '   height="200px" width="200px"></img>
+echo "<title>User Settings</title>
+        <head>User Setings</head>
+        <body bgcolor=#A9F5D0}>
+        <p>Welcome, {$_SESSION['user']}.</p>
+       
         <br><hr><br>';
 
   echo  "<form action='home_page.php' name='editAccount' method='GET'>
         First name:<input type='text' value='{$_SESSION['firstname']}' name='Accfname'>
         Last name:<input type='text' value='{$_SESSION['lastname']}' name='Acclname'>
-        Color:<input type='text' value='{$_SESSION['color']}' name='AccColor'>
-        Title:<input type='text' value='{$_SESSION['title']}' name='AccTitle'>
-        Image:<input type='text' value='{$_SESSION['image']}' name='AccImage'>
+        Email: <input type='text' value='{$_SESSION['email']}' name='AccEmail'>
+       
         <input type='submit' value='Make Changes'></form>
-
-        <center><a href='http://www.csce.uark.edu/~cwphilli/Grocery-to-Go/home_page.php'> Catalog Page </a></center>
+          
+        <center><a href='http://www.csce.uark.edu/~cphilli/Grocery-to-Go/home_page.php'> Catalog Page </a></center>
         <div>First Name: {$_SESSION['firstname']}</div>
         </body>";
 
